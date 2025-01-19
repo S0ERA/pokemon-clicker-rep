42b88af2bd6c4a06020d86560d8ae4a293336fb9
S0ERA <kereluy@gmail.com>
2025-01-20 00:22:55 +0500

    install sass and react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">YO! SOERA</div>;
+  return (
+    <div className="App">
+      <h1>SOERA</h1>
+      <p>Новый курс</p>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );

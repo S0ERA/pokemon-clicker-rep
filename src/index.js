3e342b2588de131aa4ec8d45ec0c84fb688c63cc
S0ERA <kereluy@gmail.com>
2025-01-20 19:56:44 +0500

    update scss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { BrowserRouter, Routes, Route } from "react-router";
 
+import "./scss/app.scss";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
